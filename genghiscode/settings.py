d1fae1d0db49487e40befe144f2c205b90cb2080
vitorramosdasilva <rsvitor_81@hotmail.com>
2020-06-23 21:55:13 -0300

    secret local heroku

--- genghiscode/settings.py
+++ genghiscode/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIRS = (
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
@@ -137,6 +137,9 @@ DATABASES = {
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'vpflk7%)=x9s@)gm3dxq$4*l7h378ze2a@wk56(^s1=3fwohbd'
 
 try:
     from .local_settings import *
