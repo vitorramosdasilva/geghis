a7959dbecd9b606388084036aa551c31bdebc456
vitorramosdasilva <rsvitor_81@hotmail.com>
2020-06-25 18:04:50 -0300

    adionar envio de email zoho

--- genghiscode/settings.py
+++ genghiscode/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIRS = (
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
